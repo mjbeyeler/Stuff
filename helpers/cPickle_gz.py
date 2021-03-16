94aa07720678c9c6b4379db07451bd0b10121fa0
Michael J. Beyeler <michael.beyeler@unil.ch>
2021-03-16 19:14:22 +0100

    cPickle bugfix

--- helpers/cPickle_gz.py
+++ helpers/cPickle_gz.py
@@ -2,7 +2,7 @@ import gzip as gz
 import _pickle as cPkl
 
 def dump_cPkl(title, data):
-    with gz.open(title + 'pgz', 'wb') as f: 
+    with gz.open(title + '.pgz', 'wb') as f: 
         cPkl.dump(data, f, protocol=-1)
 
 def load_cPkl(file):
