3de00b0a56a92f9239ba5f8e327e14a44028c551
Michael J. Beyeler <michael.beyeler@outlook.com>
2021-08-18 16:31:45 +0200

    fixed effects metaanlysis for gwas added

---
+++ fixed_effects_metaanalysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import stats
+
+df=pd.read_csv("~/data/metaanalysis_colaus_skipogh_RE2C.txt", delimiter=" ", names=['rsid','beta1', 'se1', 'beta2', 'se2'])
+df['weight1'] = 1/df['se1']**2
+df['weight2'] = 1/df['se2']**2
+df['beta_meta'] = (df['beta1'] * df['weight1'] + df['beta2'] * df['weight2']) / (df['weight1'] + df['weight2'])
+df['se_meta'] = np.sqrt(1/(df['weight1']+df['weight2']))
+df['t_meta'] = df['beta_meta']/df['se_meta']
+df['P'] = stats.t.sf(np.abs(df['t_meta']), df.shape[0]-1)*2 # two-sided t-test
+df[['SNP', 'beta1', 'se1', 'beta2', 'se2', 'weight1','weight2', 'beta_meta', 'se_meta', 't_meta', 'P', 'CHR', 'BP']].to_csv("out_skipogh_colaus_metaanalysis.csv",index=False)
